fixed cropping in mse calculation

metric_calculator.py
@@ -44,8 +44,8 @@ def calculate_mse(x,y,ppm):
     mses = []
     for i in range(0,ppm.shape[0]):
         i_ppm = ppm[i] 
-        max_ind = np.argmin(i_ppm[i_ppm<=4])
-        min_ind = np.argmax(i_ppm[i_ppm>=2.5])
+        max_ind = np.amax(np.where(i_ppm >= 2.5))
+        min_ind = np.amin(np.where(i_ppm <= 4))
 
         x_crop = x[i,min_ind:max_ind]
         y_crop = y[i,min_ind:max_ind]